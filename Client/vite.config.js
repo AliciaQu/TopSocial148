ce7c12dc26ee798fcac15e1731c111df0709a26b
AliciaQu <alicia.jinhui.qu@gmail.com>
2023-09-04 19:53:38 +1000

    using eslint

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import path from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-     resolves: {
+     resolve: {
          alias: {
           "@": path.resolve(__dirname,'./src')
          }
-
      }
-
 });
+
