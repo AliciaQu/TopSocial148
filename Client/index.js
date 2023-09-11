fb4ade3fb1a73cc73a5f523745883e86989ae19a
AliciaQu <alicia.jinhui.qu@gmail.com>
2023-09-11 17:59:28 +1000

    highline finish

--- Client/index.js
+++ Client/index.js
@@ -1,16 +1,24 @@
-
 import "./index.css";
-
 import { highline } from '@/js/database';
 
 const middle = document.querySelector(".main-middle");
-const middle-highline = middle.querySelector('.highline');
-const middle-highline-description = middle-highline.querySelectorAll(".description");
+const middle_highline = middle.querySelector('.highline');
+const middle_highline_description = middle_highline.querySelectorAll(".description");
 
-const middle-highline-description-img = middle-highline.querySelectorAll(".description img");
-const middle-highline-description-img = middle-highline.querySelectorAll(".description p");
+const middle_highline_description_img = middle_highline.querySelectorAll(".description img");
+const middle_highline_description_p = middle_highline.querySelectorAll(".description p");
 
-middle-highline-description-package.forEach ((ele, i) => { 
+// Description p
+middle_highline_description.forEach((ele, i) => { 
   ele.textContent = highline.description[i];
-})
+});
 
+// Background image
+middle_highline_description.forEach((ele, i) => {
+  ele.style.backgroundImage = `url("./images/highline/highline_${i+1}.jpeg")`;
+});
+
+// Profile image
+middle_highline_description_img.forEach((ele, i) => {
+  ele.src = `./images/profile/profile_${i+1}.png`;
+});
